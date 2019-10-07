7773732d1064ef538650b0ba081a41535615ffd0
mikezks <michael.zikes@intauria.com>
2019-10-07 13:05:01 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'flight-app',
+  selector: 'app-flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  title = 'Hello World!';
+  title = 'Hello Angular!';
 }
